ae06e6f40ab145c212224fd039c6a2f622cd79ec
hiromu-t396 <hiromu1193966@gmail.com>
2020-12-16 12:20:40 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, World!</h1></div>;
-  }
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+      <input type="text" id="bar" onClick={() => {console.log("I am clicked")}} />
+      </React.Fragment>
+    )
+  }  
 }
 
 export default App;
+
